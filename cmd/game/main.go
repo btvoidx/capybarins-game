dcd56270a7ac5cdda780cb076d24be685d55e812
btvoidx <maksimtumozov@gmail.com>
2024-10-29 01:53:09 +0400

    create initial directory structure
    Создал проект Go и начальную структуру директорий с cmd/game.
    В cmd/___ принято хранить только main пакеты, чтобы запускать их через go run ./cmd/___.
    Остальной код следует разбивать по пакетам как душе угодно, с учетом что наш проект нельзя импортировать (в go.mod не назван как репозиторий - go get не сможет скачать)

---
+++ cmd/game/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"game"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	game := &game.Game{}
+
+	err := ebiten.RunGame(game)
+	if err != nil && !errors.Is(err, ebiten.Termination) {
+		fmt.Fprintf(os.Stderr, "unhandled err: %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ game.go
@@ -0,0 +1,28 @@
+package game
+
+import (
+	"fmt"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct {
+	w, h int
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrintAt(screen,
+		fmt.Sprintf("window size:\n%dx%d", g.w, g.h),
+		g.w/2-35, g.h/2-12,
+	)
+}
+
+func (*Game) Update() error {
+	return nil
+}
+
+func (g *Game) Layout(w, h int) (int, int) {
+	g.w, g.h = w, h
+	return w, h
+}
